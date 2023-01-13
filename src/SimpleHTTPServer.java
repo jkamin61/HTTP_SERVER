85180d368bd70fa9e0de6dd24c3dd038b25b5455
soutdev <107152863+jkamin61@users.noreply.github.com>
2023-01-13 20:17:46 +0100

    final -v

---
+++ src/SimpleHTTPServer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class SimpleHTTPServer {
+
+    public static void main(String args[]) throws IOException {
+
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                String textOutput = "\nSimple HTTP server";
+                socket.getOutputStream()
+                        .write(httpResponse.getBytes("UTF-8"));
+                socket.getOutputStream()
+                        .write(textOutput.getBytes());
+            }
+        }
+    }
+
+}
